e83e86b89892e76e43b4f6b4aa0633b97e407e50
kuroko1t <kurosawa.yk@gmail.com>
2022-01-03 00:41:47 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="vino2onnx",
     scripts=["bin/vino2onnx"],
-    packages=[
-        "onnx",
-    ],
-    install_requires=get_requires(),
+    packages=find_packages(),
+    install_requires=["onnx"],
     version="0.1",
 )
